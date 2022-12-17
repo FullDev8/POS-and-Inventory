9da42d6b115422c77f8de5e605806944828af919
JosephDev <113911373+FullDev8@users.noreply.github.com>
2022-12-17 14:25:14 +0800

    Add files via upload

---
+++ functionality-js/product.js
@@ -0,0 +1,14 @@
+
+//Open/Close side bar
+
+const sideMenu = document.getElementById('side');
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+
+function openButton() {
+  sideMenu.style.display = 'block';
+}
+
+function closeButton() {
+  sideMenu.style.display = 'none';
+} 
